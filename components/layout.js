9b87d9088ca6b79a44980d17dae292246cc2dc1f
Joann Pan <joannlpan@gmail.com>
2022-08-02 20:10:18 -0400

    import sidebar to layout

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,14 @@ import styles from "../styles/Layout.module.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
       <Navbar />
+      <Sidebar />
       <main className={styles.main_wrapper}>{children}</main>
       <Footer />
     </div>
